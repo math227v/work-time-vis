38971e9ca7efd848635b8105e1abbe1e41c42c64
Mathias Lauridsen <math227v@gmail.com>
2022-06-21 20:27:29 +0200

    Added Bar

---
+++ src/lib/Bar.svelte
@@ -0,0 +1,37 @@
+<script>
+    export let title = Mandag;
+    
+    export let startTime = "2022-06-21T20:00:00";
+    startTime = new Date(startTime);
+    let startTimeMillis = startTime.getTime();
+    
+    export let endTime = "2022-06-21T20:30:00";
+    endTime = new Date(endTime);
+    let endTimeMillis = endTime.getTime();
+    
+    let currentTime = Date.now();
+
+    endTimeMillis = endTimeMillis - startTimeMillis;
+    currentTime = currentTime - startTimeMillis;
+
+    console.log(`Start time: ${startTimeMillis}`);
+    console.log(`End time: ${endTimeMillis}`);
+    console.log(`Current time: ${currentTime}`);
+    
+    // let barWidth = Math.floor( ( currentTime / endTimeMillis ) * 100 );
+    let barWidth = ( currentTime / endTimeMillis ) * 100;
+    console.log(`Bar width: ${barWidth}`);
+</script>
+
+<style>
+    .bar {
+        width: var(--bar-width);
+    }
+</style>
+
+<div class="bg-gray-800 m-2 p-6 pt-0">
+    <h6 class="text-white">{title}</h6>
+    <div class="bg-white h-4">
+        <div class="bar bg-green-500 h-full" style="--bar-width: {barWidth}%"></div>
+    </div>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,6 @@
+<script>
+import Bar from "$lib/Bar.svelte";
+
+</script>
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Bar title="Monday" />
